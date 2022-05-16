2ba81afbf56badbde8f40547349d651d5770f348
miari <maryna.boroday@gmail.com>
2022-05-16 10:27:28 +0200

    another minor commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.scss";
 
 function App() {
+
+  const url = "https://api.punkapi.com/v2/beers/1";
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <div>Hello!!!</div>
+        <Main beers=""/>
       </header>
     </div>
   );
---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,11 @@
+import { buildQueries } from "@testing-library/react";
+
+const Main = ({beers}) => {
+  return (
+    <div>
+      {beers}
+    </div>
+  )
+}
+
+export default Main;
