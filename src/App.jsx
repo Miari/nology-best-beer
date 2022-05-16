450962fd5e4e6974c6603974fc87a496495888e9
miari <maryna.boroday@gmail.com>
2022-05-16 09:52:10 +0200

    minor commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hello!!!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
